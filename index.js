89c3d54e211502af54114d702b844f20c93abcc2
Yuzhi <limruby97@gmail.com>
2020-11-22 16:54:19 +0800

    Day 3 - Added Eslint to enfore code style

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function(req, res){
-    return res.send('Hello');
+app.get('/', function (req, res) {
+  return res.send('Hello')
 })
 
-app.listen(3000, function(){
-    console.log('Server running at port 3000');
+app.listen(3000, function () {
+  console.log('Server running at port 3000')
 })
