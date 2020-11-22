2b8920553e38da3065282d83883249a30e9c5a55
Yuzhi <limruby97@gmail.com>
2020-11-22 16:20:53 +0800

    Day 2 - Automatic refresh added using nodemon

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function(req, res){
-    return res.send('Hello!');
+    return res.send('Hello');
 })
 
 app.listen(3000, function(){
