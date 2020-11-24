7d15c1dc824fe1548e347d9c1dd166ad1557809c
Yuzhi <limruby97@gmail.com>
2020-11-24 14:35:58 +0800

    reinstall every missing component

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
+require('./utils/db.config')
 const app = express()
+
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
   return res.send('Hello')
--- testing/makeSureServerIsRunning.js
+++ testing/makeSureServerIsRunning.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const app = require('../index')
@@ -10,6 +11,7 @@ describe('Make sure that status is 200', () => {
   it('should return 200', done => {
     chai.request(app)
       .get('/')
+      // eslint-disable-next-line node/handle-callback-err
       .end((err, res) => {
         res.should.have.status(200)
         res.body.should.be.a('object')
