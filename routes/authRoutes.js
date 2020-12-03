562042ad19f84bb3747d807c554980a5764eb3b9
Yuzhi <limruby97@gmail.com>
2020-12-04 02:44:19 +0800

    Day 15- Login page added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -51,4 +51,24 @@ router.post('/register', async (req, res) => {
   }
 })
 
+/**
+ * Shows page for user login
+ */
+router.get('/login', (req, res) => {
+  return res.render('login', { message: {}, formData: {}, errors: {} })
+})
+/**
+ * User log in
+ */
+router.post('/login', (req, res) => {
+  return res.render('login', {
+    message: {
+      type: 'success',
+      body: 'Login success'
+    },
+    formData: {},
+    errors: {}
+  })
+})
+
 module.exports = router
