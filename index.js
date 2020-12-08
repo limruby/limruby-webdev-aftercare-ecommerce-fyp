5e123190d2baa3e1aa022509f6a6a20564020e58
Yuzhi <limruby97@gmail.com>
2020-12-08 15:47:57 +0800

    Day 28 - Access logged in user to views

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const logger = require('morgan')
 require('./utils/db.config')
 const MongoStore = require('connect-mongo')(session)
-const mongoDBConnection = require('./utils/db.config')
+const mongoDbConnection = require('./utils/db.config')
 const passport = require('passport')
 require('./utils/authStrategies/localStrategy')
 const authMiddleware = require('./middlewares/authMiddleware')
@@ -16,15 +16,15 @@ const config = require('./utils/config')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'pug')
-// app.set('trust proxy', 1) // trust first proxy
+// app.set('trust proxy', 1)
 app.use(session({
-// encrypting session data, secret generated randomly from node crypto
+  // encrypting session data, secret generated randomly from node crypto
   secret: 'dbef350b8b247452a132cbdd04c7bea371c87e95',
   resave: false,
   saveUninitialized: true,
   // false because we use localhost, set true for HTTPS and certificate installation in browser
   cookie: { secure: false },
-  store: new MongoStore({ mongooseConnection: mongoDBConnection })
+  store: new MongoStore({ mongooseConnection: mongoDbConnection })
 }))
 app.use(express.static('public'))
 app.use(logger('dev'))
@@ -35,8 +35,8 @@ app.use(passport.session())
  * Global middleware to make logged in user available to the views
  */
 app.use((req, res, next) => {
-  res.locals.users = req.isAuthenticated() ? req.user : null
-  next()
+  res.locals.user = req.isAuthenticated() ? req.user : null
+  return next()
 })
 
 /**
@@ -52,7 +52,6 @@ app.use('/', authRoutes)
 // app.use('/public', express.static('public'))
 
 app.get('/', flasherMiddleware, (req, res) => {
-  console.log(req.method)
   return res.render('index')
 })
 
